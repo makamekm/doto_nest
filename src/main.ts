636fea500357890f1dd10f2bf191937472d846bc
Максим Карпов <makame@mail.ru>
2019-04-04 19:05:38 +0200

    Express for serverless

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 import Next from 'next';
 import { RenderModule } from 'nest-next';
 import { ErrorFilter } from './errors.filter';
@@ -8,10 +10,14 @@ async function bootstrap() {
   const dev = process.env.NODE_ENV !== 'production';
   const app = Next({ dev });
   await app.prepare();
-  const server = await NestFactory.create(AppModule);
+  const expressApp = express();
+  const adapter = new ExpressAdapter(expressApp);
+  const server = await NestFactory.create(AppModule, adapter);
+  await server.enableCors();
+  await server.init();
   const renderer = server.get(RenderModule);
   renderer.register(server, app);
   server.useGlobalFilters(new ErrorFilter());
-  await server.listen(process.env.PORT || 3000);
+  expressApp.listen(process.env.PORT || 3000);
 }
 bootstrap();

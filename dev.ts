46b7b8e6dc0df291c728526f03b10db9cbb0eda8
karpovm <maxim.karpov@ubs.com>
2019-04-08 08:44:00 -0400

    bootstrap dev mode

--- dev.ts
+++ dev.ts
@@ -1,3 +1,20 @@
 import server from './mt/main';
 
+import * as next from 'next';
+
+const promise = bootstrap();
+
+async function bootstrap() {
+    const dev = process.env.NODE_ENV !== 'production';
+    const app = next({ dev });
+    await app.prepare();
+    const handle = app.getRequestHandler();
+    return handle;
+}
+
+server.get('*', async (req, res) => {
+    const handle = await promise;
+    return handle(req, res);
+});
+
 server.listen(process.env.PORT || 3000);
--- mt/app.module.ts
+++ mt/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RenderModule } from 'nest-next';
 
 @Module({
-  imports: [RenderModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })

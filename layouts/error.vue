0f496ce085d93490cd0efb6b5d208e04ef69de1d
Максим Карпов <makame@mail.ru>
2019-04-22 21:48:54 +0200

    Fix error transition

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,25 +1,37 @@
 <template>
   <section class="gl-layout-margin">
-    <section class="hero is-light">
-      <div class="hero-body">
-        <div class="container has-text-centered">
-          <h1 class="title">
-            {{ error.statusCode }}
-          </h1>
-          <h2 class="subtitle">
-            {{ error.message }}
-          </h2>
-          <button class="button is-primary is-medium" to="/" @click="reload">
-            Refresh
-          </button>
+    <transition name="fade">
+      <section class="hero is-light" v-if="show">
+        <div class="hero-body">
+          <div class="container has-text-centered">
+            <h1 class="title">
+              {{ error.statusCode }}
+            </h1>
+            <h2 class="subtitle">
+              {{ error.message }}
+            </h2>
+            <button class="button is-primary is-medium" to="/" @click="reload">
+              Refresh
+            </button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </transition>
   </section>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    show: false,
+  }),
+  mounted: function() {
+    if (process.client) {
+      window.setTimeout(() => this.show = true, 300);
+    } else {
+      this.show = true;
+    }
+  },
   props: ['error'],
   methods: {
     reload() {
@@ -28,12 +40,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.gl-layout-margin {
-  position: absolute;
-  top: 0;
-  right: $column-gap / 2;
-  left: $column-gap / 2;
-}
-</style>

9bbe345ce51d61208f6380128a04ac49659610aa
Максим Карпов <makame@mail.ru>
2019-04-27 11:17:29 +0200

    Optimize

--- api/main.ts
+++ api/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
 

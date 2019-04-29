a48f31217d919b8d33cb44621e357fd5bf318b22
Максим Карпов <makame@mail.ru>
2019-04-30 01:32:09 +0200

    Form-group init

---
+++ components/form-grid.vue
@@ -0,0 +1,162 @@
+<template>
+  <div :class="classnames" :style="{ cursor, userSelect }" @mousedown="onMouseDown">
+    <template v-for="col in cols">
+      <slot :name="col" :col="col"></slot>
+      <div class="multipane-resizer" v-bind:key="col" v-if="col != cols"/>
+    </template>
+  </div>
+</template>
+
+<style lang="scss">
+.multipane {
+  display: flex;
+  &.layout-h {
+    flex-direction: column;
+  }
+  &.layout-v {
+    flex-direction: row;
+  }
+}
+.multipane > div {
+  position: relative;
+  z-index: 1;
+}
+.multipane-resizer {
+  display: block;
+  position: relative;
+  z-index: 2;
+}
+.layout-h > .multipane-resizer {
+  width: 100%;
+  height: 10px;
+  margin-top: -10px;
+  top: 5px;
+  cursor: row-resize;
+}
+.layout-v > .multipane-resizer {
+  width: 10px;
+  height: 100%;
+  margin-left: -10px;
+  left: 5px;
+  cursor: col-resize;
+}
+</style>
+
+<script>
+const LAYOUT_HORIZONTAL = 'horizontal';
+const LAYOUT_VERTICAL = 'vertical';
+
+export default {
+  name: 'multipane',
+
+  props: {
+    cols: {
+      type: Number,
+    },
+    layout: {
+      type: String,
+      default: LAYOUT_VERTICAL,
+    },
+  },
+
+  data() {
+    return {
+      isResizing: false,
+    };
+  },
+
+  computed: {
+    classnames() {
+      return [
+        'multipane',
+        'layout-' + this.layout.slice(0, 1),
+        this.isResizing ? 'is-resizing' : '',
+      ];
+    },
+    cursor() {
+      return this.isResizing
+        ? this.layout == LAYOUT_VERTICAL ? 'col-resize' : 'row-resize'
+        : '';
+    },
+    userSelect() {
+      return this.isResizing ? 'none' : '';
+    },
+  },
+
+  methods: {
+    onMouseDown({ target: resizer, pageX: initialPageX, pageY: initialPageY }) {
+      if (resizer.className && resizer.className.match('multipane-resizer')) {
+        let self = this;
+        let { $el: container, layout } = self;
+
+        let pane = resizer.previousElementSibling;
+        let {
+          offsetWidth: initialPaneWidth,
+          offsetHeight: initialPaneHeight,
+        } = pane;
+
+        let usePercentage = !!(pane.style.width + '').match('%');
+
+        const { addEventListener, removeEventListener } = window;
+
+        const resize = (initialSize, offset = 0) => {
+          if (layout == LAYOUT_VERTICAL) {
+            let containerWidth = container.clientWidth;
+            let paneWidth = initialSize + offset;
+
+            return (pane.style.width = usePercentage
+              ? paneWidth / containerWidth * 100 + '%'
+              : paneWidth + 'px');
+          }
+
+          if (layout == LAYOUT_HORIZONTAL) {
+            let containerHeight = container.clientHeight;
+            let paneHeight = initialSize + offset;
+
+            return (pane.style.height = usePercentage
+              ? paneHeight / containerHeight * 100 + '%'
+              : paneHeight + 'px');
+          }
+        };
+
+        // This adds is-resizing class to container
+        self.isResizing = true;
+
+        // Resize once to get current computed size
+        let size = resize();
+
+        // Trigger paneResizeStart event
+        self.$emit('paneResizeStart', pane, resizer, size);
+
+        const onMouseMove = function({ pageX, pageY }) {
+          size =
+            layout == LAYOUT_VERTICAL
+              ? resize(initialPaneWidth, pageX - initialPageX)
+              : resize(initialPaneHeight, pageY - initialPageY);
+
+          self.$emit('paneResize', pane, resizer, size);
+        };
+
+        const onMouseUp = function() {
+          // Run resize one more time to set computed width/height.
+          size =
+            layout == LAYOUT_VERTICAL
+              ? resize(pane.clientWidth)
+              : resize(pane.clientHeight);
+
+          // This removes is-resizing class to container
+          self.isResizing = false;
+
+          removeEventListener('mousemove', onMouseMove);
+          removeEventListener('mouseup', onMouseUp);
+
+          self.$emit('paneResizeStop', pane, resizer, size);
+        };
+
+        addEventListener('mousemove', onMouseMove);
+        addEventListener('mouseup', onMouseUp);
+      }
+    },
+  },
+};
+</script>
---
+++ pages/grid.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="gl-layout-margin">
+    <form-grid :cols="2">
+      <template v-slot:1>
+        <div>
+          Fdfdf
+        </div>
+      </template>
+      <template v-slot:2>
+        <div>
+          fghfghf
+        </div>
+      </template>
+    </form-grid>
+  </div>
+</template>
+
+<script lang="ts">
+import FormGrid from "~/components/form-grid.vue";
+
+export default {
+  middleware: ['auth'],
+  auth: false,
+  data: () => ({
+  }),
+  components: {
+    FormGrid,
+  },
+}
+</script>
